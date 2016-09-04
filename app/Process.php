8f95bcd536e49c67d4ddad8c47138ddb8a98c641
Kirill <suhorukih@mail.ru>
2016-09-04 16:56:31 +0300

    обертка над созданием линукс процесса

---
+++ app/Process.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace app\models;
+use yii;
+class Process{
+    private $pid;
+    private $command;
+
+    public function __construct($cl=false){
+        if ($cl != false){
+            $this->command = $cl;
+            $this->runCom();
+        }
+    }
+    private function runCom(){
+        $command = 'nohup '.$this->command.' > /dev/null 2>&1 & echo $!';
+        exec($command ,$op);
+        $this->pid = (int)$op[0];
+    }
+
+    public function setPid($pid){
+        $this->pid = $pid;
+    }
+
+    public function getPid(){
+        return $this->pid;
+    }
+
+    public function status(){
+        $command = 'ps -p '.$this->pid;
+        exec($command,$op);
+        if (!isset($op[1]))return false;
+        else return true;
+    }
+
+    public function start(){
+        if ($this->command != '')$this->runCom();
+        else return true;
+    }
+
+    public function stop(){
+
+        $command = 'kill '.$this->pid;
+
+        $sigterm = 15;
+        $result = posix_kill($this->pid,$sigterm);
+
+        return $result;
+    }
+}
+
+?>
