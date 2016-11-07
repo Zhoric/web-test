144ed12607113fa5c83b8ea5fc1f545eb66f49e3
nyanjii <khiv.natalia@gmail.com>
2016-11-07 10:47:31 +0300

    Рефакторинг кода дисциплин

--- public/js/admin/disciplines.js
+++ public/js/admin/disciplines.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             var self = this;
 
             self.disciplines = ko.observableArray([]);
-            self.current = ko.observable({
+            self.current = {
                 discipline: ko.observable({
                     id: ko.observable(0),
                     name: ko.observable(''),
@@ -24,29 +24,29 @@ $(document).ready(function(){
                     id: ko.observable(0),
                     name: ko.observable('')
                 })
-            });
-            self.filter = ko.observable({
+            };
+            self.filter = {
                 discipline: ko.observable(''),
                 profile : ko.observable()
-            });
-            self.toggleCurrent = ko.observable({
+            };
+            self.toggleCurrent = {
                 fill: function(data){
-                    self.current().discipline()
+                    self.current.discipline()
                         .id(data.id())
                         .name(data.name())
                         .abbreviation(data.abbreviation())
                         .description(data.description());
                 },
                 empty: function(){
-                    self.current().discipline()
+                    self.current.discipline()
                         .id(0)
                         .name('')
                         .abbreviation('')
                         .description('');
-                    self.current().profile().selected([]);
+                    self.current.profile().selected([]);
                 },
                 stringify: function(){
-                    var edit = self.current().discipline();
+                    var edit = self.current.discipline();
                     var profiles = [];
                     var forpost = {
                         name: edit.name(),
@@ -54,16 +54,16 @@ $(document).ready(function(){
                         description: edit.description()
                     };
                     self.mode() === 'edit' ? forpost.id = edit.id() : null;
-                    self.current().profile().selected().forEach(function(item){
+                    self.current.profile().selected().forEach(function(item){
                         profiles.push(item.id());
                     });
                     return JSON.stringify({discipline: forpost, profileIds: profiles});
                 },
                 setInitialProfiles: function(){
-                    var discipline = self.current().profile().discipline();
-                    var profiles = self.current().profile().profiles;
-                    var selected = self.current().profile().selected;
-                    var received = self.current().profile().received;
+                    var discipline = self.current.profile().discipline();
+                    var profiles = self.current.profile().profiles;
+                    var selected = self.current.profile().selected;
+                    var received = self.current.profile().received;
                     selected([]);
                     if (discipline.length){
                         discipline.forEach(function(disc){
@@ -78,28 +78,28 @@ $(document).ready(function(){
                         });
                     }
                 }
-            });
-            self.pagination = ko.observable({
+            };
+            self.pagination = {
                 currentPage: ko.observable(1),
                 pageSize: ko.observable(10),
                 itemsCount: ko.observable(1),
                 totalPages: ko.observable(1),
 
                 selectPage: function(page){
-                    self.pagination().currentPage(page);
-                    self.get().disciplines();
+                    self.pagination.currentPage(page);
+                    self.get.disciplines();
                 },
                 dotsVisible: function(index){
-                    var total = self.pagination().totalPages();
-                    var current = self.pagination().currentPage();
+                    var total = self.pagination.totalPages();
+                    var current = self.pagination.currentPage();
                     if (total > 11 && index == total-1 && index > current + 2  ||total > 11 && index == current - 1 && index > 3)  {
                         return true;
                     }
                     return false;
                 },
                 pageNumberVisible: function(index){
-                    var total = self.pagination().totalPages();
-                    var current = self.pagination().currentPage();
+                    var total = self.pagination.totalPages();
+                    var current = self.pagination.currentPage();
                     if (total < 12 ||
                         index > (current - 2) && index < (current + 2) ||
                         index > total - 2 ||
@@ -108,27 +108,27 @@ $(document).ready(function(){
                     }
                     return false;
                 },
-            });
+            };
             self.mode = ko.observable('none');
-            self.csed = ko.observable({
+            self.csed = {
                 show: function(data){
-                    if (self.mode() === 'none' || self.current().discipline().id() !== data.id()){
+                    if (self.mode() === 'none' || self.current.discipline().id() !== data.id()){
                         self.mode('info');
-                        self.get().disciplineProfiles();
-                        self.toggleCurrent().fill(data);
-                        self.get().themes();
+                        self.get.disciplineProfiles();
+                        self.toggleCurrent.fill(data);
+                        self.get.themes();
                         return;
                     }
                     self.mode('none');
-                    self.toggleCurrent().empty();
+                    self.toggleCurrent.empty();
                 },
                 startAdd: function(){
-                    self.toggleCurrent().empty();
+                    self.toggleCurrent.empty();
                     self.mode() === 'add' ? self.mode('none') : self.mode('add');
                 },
                 startUpdate: function(){
                     self.mode('edit');
-                    self.toggleCurrent().setInitialProfiles();
+                    self.toggleCurrent.setInitialProfiles();
                 },
                 startRemove: function(){
                     self.mode('delete');
@@ -136,110 +136,110 @@ $(document).ready(function(){
                 },
                 update: function(){
                     var url = self.mode() === 'add' ? '/api/disciplines/create' : '/api/disciplines/update';
-                    var json = self.toggleCurrent().stringify();
+                    var json = self.toggleCurrent.stringify();
                     console.log(url + ' : ' + json);
                     self.post(url, json);
                 },
                 remove: function(){
                     self.toggleModal('#delete-modal', 'close');
-                    var url = '/api/disciplines/delete/' + self.current().discipline().id();
+                    var url = '/api/disciplines/delete/' + self.current.discipline().id();
                     self.post(url, '');
                 },
                 cancel: function(){
                     if (self.mode() === 'add'){
                         self.mode('none');
-                        self.toggleCurrent().empty();
+                        self.toggleCurrent.empty();
                         return;
                     }
                     self.mode('info');
                 },
-                theme: ko.observable({
+                theme: {
                     startAdd: function(){
                         console.log('add theme');
-                        self.current().theme().id(0).name('');
+                        self.current.theme().id(0).name('');
                         self.toggleModal('#add-theme-modal', '');
                     },
                     add: function(){
                         var url = '/api/disciplines/themes/create';
                         var json = JSON.stringify({
                             theme: {
-                                name: self.current().theme().name()
+                                name: self.current.theme().name()
                             },
-                            disciplineId: self.current().discipline().id()
+                            disciplineId: self.current.discipline().id()
                         });
                         console.log(url + ' : ' + json);
                         $.post(url, json, function(){
                             self.toggleModal('#add-theme-modal', 'close');
-                            self.get().themes();
+                            self.get.themes();
                         });
                     },
                     startRemove: function(data){
                         self.toggleModal('#remove-theme-modal', '');
-                        self.current().theme().id(data.id()).name(data.name());
+                        self.current.theme().id(data.id()).name(data.name());
 
                     },
                     remove: function(){
-                        var url = '/api/disciplines/themes/delete/' + self.current().theme().id();
+                        var url = '/api/disciplines/themes/delete/' + self.current.theme().id();
                         $.post(url, function(){
                             self.toggleModal('#remove-theme-modal', 'close');
-                            self.get().themes();
+                            self.get.themes();
                         });
                     }
-                })
-            });
+                }
+            };
 
-            self.moveTo = ko.observable({
+            self.moveTo = {
                 theme: function(data){
                     window.location.href = '/admin/theme/' + data.id();
                 },
                 tests: function(data){
                     window.location.href = '/admin/tests/' + data.id();
                 }
-            });
-            self.get = ko.observable({
+            };
+            self.get = {
                 disciplines: function(profileId){
-                    var filter = self.filter();
+                    var filter = self.filter;
                     var profile = 'profile=' + (filter.profile() ? filter.profile().id() : '');
                     var name = 'name=' + filter.discipline();
-                    var page = 'page=' + self.pagination().currentPage();
-                    var pageSize = 'pageSize=' + self.pagination().pageSize();
+                    var page = 'page=' + self.pagination.currentPage();
+                    var pageSize = 'pageSize=' + self.pagination.pageSize();
                     var url = '/api/disciplines/show?' + page + '&' + pageSize + '&' + name + '&' + profile;
 
                     $.get(url, function(response){
                         var result = ko.mapping.fromJSON(response);
                         self.disciplines(result.data());
-                        self.pagination().itemsCount(result.count());
+                        self.pagination.itemsCount(result.count());
                     });
                 },
                 disciplineProfiles: function(){
-                    var id = self.current().discipline().id();
+                    var id = self.current.discipline().id();
                     if (id){
                         $.get('/api/disciplines/' + id + '/profiles', function(response){
-                            self.current().profile().discipline(ko.mapping.fromJSON(response)());
+                            self.current.profile().discipline(ko.mapping.fromJSON(response)());
                         });
                     }
                 },
                 profiles: function(){
                     $.get('/api/profiles', function(response){
-                        self.current().profile().profiles(ko.mapping.fromJSON(response)());
+                        self.current.profile().profiles(ko.mapping.fromJSON(response)());
                     });
                 },
                 themes: function(){
-                    var url = '/api/disciplines/' + self.current().discipline().id() +'/themes';
+                    var url = '/api/disciplines/' + self.current.discipline().id() +'/themes';
                     $.get(url, function(response){
-                        self.current().themes(ko.mapping.fromJSON(response)());
+                        self.current.themes(ko.mapping.fromJSON(response)());
                     });
                 }
-            });
-            self.get().disciplines();
-            self.get().profiles();
+            };
+            self.get.disciplines();
+            self.get.profiles();
 
 
             self.post = function(url, json){
                 $.post(url, json, function(result){
                     self.mode('none');
-                    self.toggleCurrent().empty();
-                    self.get().disciplines();
+                    self.toggleCurrent.empty();
+                    self.get.disciplines();
                 });
             };
             self.toggleModal = function(selector, action){
@@ -247,18 +247,18 @@ $(document).ready(function(){
             };
 
             // SUBSCRIPTIONS
-            self.pagination().itemsCount.subscribe(function(value){
+            self.pagination.itemsCount.subscribe(function(value){
                 if (value){
-                    self.pagination().totalPages(Math.ceil(
-                        value/self.pagination().pageSize()
+                    self.pagination.totalPages(Math.ceil(
+                        value/self.pagination.pageSize()
                     ));
                 }
             });
-            self.filter().discipline.subscribe(function(){
-                self.get().disciplines();
+            self.filter.discipline.subscribe(function(){
+                self.get.disciplines();
             });
-            self.filter().profile.subscribe(function(){
-                self.get().disciplines();
+            self.filter.profile.subscribe(function(){
+                self.get.disciplines();
             });
 
             return {
--- resources/views/admin/disciplines.blade.php
+++ resources/views/admin/disciplines.blade.php
@@ -9,44 +9,44 @@
     <div class="filter">
         <div>
             <label>Название дисциплины</label></br>
-            <input type="text" data-bind="value: $root.filter().discipline, valueUpdate: 'keyup'">
+            <input type="text" data-bind="value: $root.filter.discipline, valueUpdate: 'keyup'">
         </div>
         <div>
             <label>Направление</label></br>
-            <select data-bind="options: $root.current().profile().profiles,
+            <select data-bind="options: $root.current.profile().profiles,
                        optionsText: 'name',
-                       value: $root.filter().profile,
+                       value: $root.filter.profile,
                        optionsCaption: 'Выберите профиль'"></select>
         </div>
     </div>
     <div class="org-accordion">
-        <div data-bind="click: $root.csed().startAdd" class="org-item">
+        <div data-bind="click: $root.csed.startAdd" class="org-item">
             <span class="fa">&#xf067;</span>
         </div>
         <!-- ko if: $root.mode() === 'add'-->
-            <div data-bind="template: {name: 'show-details', data: $root.current().discipline}"></div>
+            <div data-bind="template: {name: 'show-details', data: $root.current.discipline}"></div>
         <!-- /ko -->
         <!-- ko foreach: disciplines -->
-            <div class="org-item" data-bind="text: name, click: $root.csed().show"></div>
-            <!-- ko if: $root.mode() !== 'none' && $data.id() === $root.current().discipline().id()-->
-                <div data-bind="template: {name: 'show-details', data: $root.current().discipline}"></div>
+            <div class="org-item" data-bind="text: name, click: $root.csed.show"></div>
+            <!-- ko if: $root.mode() !== 'none' && $data.id() === $root.current.discipline().id()-->
+                <div data-bind="template: {name: 'show-details', data: $root.current.discipline}"></div>
             <!-- /ko -->
         <!-- /ko -->
     </div>
     <div class="pager-wrap">
-        <!-- ko if: ($root.pagination().totalPages()) > 0 -->
+        <!-- ko if: ($root.pagination.totalPages()) > 0 -->
         <div class="pager">
-            <!-- ko ifnot: $root.pagination().currentPage() == 1 -->
-            <button class="" data-bind="click: $root.pagination().selectPage.bind($data, 1)">&lsaquo;&lsaquo;</button>
-            <button class="" data-bind="click: $root.pagination().selectPage.bind($data, ($root.pagination().currentPage() - 1))">&lsaquo;</button>
+            <!-- ko ifnot: $root.pagination.currentPage() == 1 -->
+            <button class="" data-bind="click: $root.pagination.selectPage.bind($data, 1)">&lsaquo;&lsaquo;</button>
+            <button class="" data-bind="click: $root.pagination.selectPage.bind($data, ($root.pagination.currentPage() - 1))">&lsaquo;</button>
             <!-- /ko -->
-            <!-- ko foreach: new Array($root.pagination().totalPages()) -->
-            <span data-bind="visible: $root.pagination().dotsVisible($index() + 1)">...</span>
-            <button class="" data-bind="click: $root.pagination().selectPage.bind($data, ($index()+1)), text: ($index()+1), visible: $root.pagination().pageNumberVisible($index() + 1), css: {current: ($index() + 1) == $root.pagination().currentPage()}"></button>
+            <!-- ko foreach: new Array($root.pagination.totalPages()) -->
+            <span data-bind="visible: $root.pagination.dotsVisible($index() + 1)">...</span>
+            <button class="" data-bind="click: $root.pagination.selectPage.bind($data, ($index()+1)), text: ($index()+1), visible: $root.pagination.pageNumberVisible($index() + 1), css: {current: ($index() + 1) == $root.pagination.currentPage()}"></button>
             <!-- /ko -->
-            <!-- ko ifnot: $root.pagination().currentPage() == $root.pagination().totalPages() -->
-            <button class="" data-bind="click: $root.pagination().selectPage.bind($data, ($root.pagination().currentPage() + 1))">&rsaquo;</button>
-            <button class="" data-bind="click: $root.pagination().selectPage.bind($data, $root.pagination().totalPages())">&rsaquo;&rsaquo;</button>
+            <!-- ko ifnot: $root.pagination.currentPage() == $root.pagination.totalPages() -->
+            <button class="" data-bind="click: $root.pagination.selectPage.bind($data, ($root.pagination.currentPage() + 1))">&rsaquo;</button>
+            <button class="" data-bind="click: $root.pagination.selectPage.bind($data, $root.pagination.totalPages())">&rsaquo;&rsaquo;</button>
             <!-- /ko -->
         </div>
         <!-- /ko -->
@@ -58,8 +58,8 @@
         <div>
             <div><span>Удалить выбранную дисциплину?</span></div>
             <div>
-                <button data-bind="click: $root.csed().remove" class="fa">&#xf00c;</button>
-                <button data-bind="click: $root.csed().cancel" class="fa danger arcticmodal-close">&#xf00d;</button>
+                <button data-bind="click: $root.csed.remove" class="fa">&#xf00c;</button>
+                <button data-bind="click: $root.csed.cancel" class="fa danger arcticmodal-close">&#xf00d;</button>
             </div>
         </div>
     </div>
@@ -69,7 +69,7 @@
         <div>
             <div><span>Удалить выбранную тему?</span></div>
             <div>
-                <button data-bind="click: $root.csed().theme().remove" class="fa">&#xf00c;</button>
+                <button data-bind="click: $root.csed.theme.remove" class="fa">&#xf00c;</button>
                 <button class="fa danger arcticmodal-close">&#xf00d;</button>
             </div>
         </div>
@@ -81,10 +81,10 @@
             <div><span>Добавление темы</span></div>
             <div>
                 <label>Название</label>
-                <input type="text" data-bind="value: $root.current().theme().name">
+                <input type="text" data-bind="value: $root.current.theme().name">
             </div>
             <div>
-                <button data-bind="click: $root.csed().theme().add" class="fa">&#xf00c;</button>
+                <button data-bind="click: $root.csed.theme.add" class="fa">&#xf00c;</button>
                 <button class="fa danger arcticmodal-close">&#xf00d;</button>
             </div>
         </div>
@@ -106,11 +106,11 @@
                 <tr><th>#</th><th>Темы</th><th>Действия</th></tr>
                 </thead>
                 <tbody>
-                <!-- ko foreach: $root.current().themes-->
+                <!-- ko foreach: $root.current.themes-->
                 <tr>
                     <td data-bind="text: $index()+1"></td>
-                    <td><a data-bind="text: name, click: $root.moveTo().theme"></a></td>
-                    <td><button data-bind="click: $root.csed().theme().startRemove" class="fa danger">&#xf014;</button></td>
+                    <td><a data-bind="text: name, click: $root.moveTo.theme"></a></td>
+                    <td><button data-bind="click: $root.csed.theme.startRemove" class="fa danger">&#xf014;</button></td>
                 </tr>
                 <!-- /ko -->
                 </tbody>
@@ -131,12 +131,12 @@
         </div>
         <div>
         <i>
-            <button class="move" data-bind="click: $root.csed().theme().startAdd"><span class="fa">&#xf067;</span>&nbsp;Добавить тему</button>
-            <button class="move" data-bind="click: $root.moveTo().tests"><span class="fa">&#xf044;</span>&nbsp;Тесты</button>
+            <button class="move" data-bind="click: $root.csed.theme.startAdd"><span class="fa">&#xf067;</span>&nbsp;Добавить тему</button>
+            <button class="move" data-bind="click: $root.moveTo.tests"><span class="fa">&#xf044;</span>&nbsp;Тесты</button>
         </i>
         <i>
-            <button data-bind="click: $root.csed().startUpdate" class="fa">&#xf040;</button>
-            <button data-bind="click: $root.csed().startRemove" class="fa danger">&#xf014;</button>
+            <button data-bind="click: $root.csed.startUpdate" class="fa">&#xf040;</button>
+            <button data-bind="click: $root.csed.startRemove" class="fa danger">&#xf014;</button>
         </i>
         </div>
     </div>
@@ -153,13 +153,13 @@
         </div>
         <div>
             <label>Профили</label></br>
-            <!-- ko with: $root.current().profile() -->
+            <!-- ko with: $root.current.profile() -->
             <select data-bind="options: profiles, optionsText: 'fullname',  selectedOptions: selected" size="4" multiple="true"></select>
             <!-- /ko -->
         </div>
         <div class="float-btn-group">
-            <button data-bind="click: $root.csed().update" class="fa">&#xf00c;</button>
-            <button data-bind="click: $root.csed().cancel" class="fa danger">&#xf00d;</button>
+            <button data-bind="click: $root.csed.update" class="fa">&#xf00c;</button>
+            <button data-bind="click: $root.csed.cancel" class="fa danger">&#xf00d;</button>
         </div>
     </div>
 </script>
