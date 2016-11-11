b855d179f36affa992055a984b5c9cc386f608c4
BikeInventor <niksev27@yandex.ru>
2016-11-11 11:20:59 +0300

    HotFix пропавшего юза

--- app/Http/Controllers/DisciplineController.php
+++ app/Http/Controllers/DisciplineController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Discipline;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Managers\DisciplineManager;
 use Managers\ScheduleManager;
 use Theme;
