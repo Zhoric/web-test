76755d08163e7991609077b400d1aa8dcd118fef
nyanjii <khiv.natalia@gmail.com>
2016-11-12 09:18:20 +0300

    Исправлены стилевые недочеты обаласти для студента

--- resources/views/student/test.blade.php
+++ resources/views/student/test.blade.php
@@ -54,11 +54,12 @@
                 <h2>Результат теста</h2>
                 <div class="result-text">
                     <!-- ko if: $root.current.testResult().mark() -->
-                        <span>Ваш результат составляет: <span data-bind="text: $root.current.testResult().mark()"></span>/100 баллов.</span>
+                        <span class="text-middle">Ваш результат составляет: <span data-bind="text: $root.current.testResult().mark()"></span>/100 баллов.</span>
                     <!-- /ko -->
                     <!-- ko if: !$root.current.testResult().mark() -->
                     <span>Результат вашего теста вы сможете узнать после того, как преподаватель проверит ваши ответы на открытые вопросы.</span>
                     <!-- /ko -->
+                    <span class="date" data-bind="text: $root.current.testResult().dateTime()"></span>
                 </div>
                 <button data-bind="click: $root.actions.goHome">Вернуться на главную</button>
             </div>
