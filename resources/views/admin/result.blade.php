4dc07bd31e85b66319f76f1b4a894806af38e6d8
nyanjii <khiv.natalia@gmail.com>
2016-12-10 14:05:53 +0300

    Редизайн карточки результатов одной попытки

--- resources/views/admin/result.blade.php
+++ resources/views/admin/result.blade.php
@@ -14,101 +14,111 @@
 
 @section('content')
 <div class="content result-details">
-    <div class="org-info" data-bind="if: current.result()">
-        <div class="main-info width100" data-bind="with: current.result">
-            <div class="name">
-                <label>ФИО студента</label></br>
-                <span data-bind="text: user.lastName"></span>
-                <span data-bind="text: user.firstName"></span>
-                <span data-bind="text: user.patronymic"></span>
-            </div>
-            <div class="date">
-                <label>Дата прохождения теста</label></br>
-                <span data-bind="text: dateTime.date"></span>
+    <div class="layer" data-bind="with: current.result">
+        <div class="details-row">
+            <div class="details-column width-25p">
+                <div class="details-row">
+                    <div class="details-column width-100p">
+                        <label class="title">Дата прохождения теста</label>
+                        <span class="info" data-bind="text: dateTime.date"></span>
+                    </div>
+                </div>
+                <div class="details-row">
+                    <div class="details-column width-100p">
+                        <label class="title">Номер попытки &nbsp;<span class="clickable" data-bind="if: attempt() > 1, click: $root.actions.results.view">(Все попытки)</span></label>
+                        <span class="info" data-bind="text: attempt"></span>
+                        <span class="info">/</span>
+                        <span class="info" data-bind="text: $root.current.attempts()"></span>
+                    </div>
+                </div>
             </div>
             <!-- ko if: $root.current.test -->
                 <!-- ko with: $root.current.test -->
-                <div class="test">
-                    <label>Тест</label></br>
-                    <span data-bind="text: subject"></span>
-                </div>
-                <div class="discipline">
-                    <label>Дисциплина</label></br>
-                    <span data-bind="text: disciplineName"></span>
+                <div class="details-column width-38p">
+                    <div class="details-row">
+                        <div class="details-column width-100p">
+                            <label class="title">Тест</label>
+                            <span class="info" data-bind="text: subject"></span>
+                        </div>
+                    </div>
+                    <div class="details-row">
+                        <div class="details-column width-100p">
+                            <label class="title">Дисциплина</label>
+                            <span class="info" data-bind="text: disciplineName"></span>
+                        </div>
+                    </div>
                 </div>
                 <!-- /ko -->
             <!-- /ko -->
-            <div class="mark">
-                <label>Оценка</label></br>
-                <!-- ko if: mark() !== null -->
-                <span data-bind="text: mark"></span>
-                <span>/100</span>
-                <!-- /ko -->
-                <span class="not-ok" data-bind="if: mark() === null">Требуется проверка</span>
-            </div>
-            <div class="attempts">
-                <label>Номер попытки &nbsp;<span class="clickable" data-bind="if: attempt() > 1, click: $root.actions.results.view">(Все попытки)</span></label></br>
-                <span data-bind="text: attempt"></span>
-                <span>/</span>
-                <span data-bind="text: $root.current.attempts()"></span>
+            <div class="details-column width-30p">
+                <div class="details-row">
+                    <div class="details-column width-100p">
+                        <label class="title">ФИО студента</label>
+                        <span class="info" data-bind="text: user.lastName() + ' ' +
+                                                            user.firstName() + ' ' +
+                                                            user.patronymic()"></span>
+                    </div>
+                </div>
+                <div class="details-row">
+                    <div class="details-column width-100p">
+                        <label class="title">Оценка</label>
+                        <!-- ko if: mark() !== null -->
+                        <span class="radio-important" data-bind="text: mark() + '/100'"></span>
+                        <!-- /ko -->
+                        <span class="radio-negative" data-bind="if: mark() === null">Требуется проверка</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-    <!-- ko if: current.answers().length -->
-    <div class="answers" data-bind="foreach: current.answers">
-        <div class="answer" data-bind="click: $root.actions.answer.show, css: {'current': $root.current.answer().id() === id()}">
-            <span data-bind="text: $root.actions.answer.fit.question($data)"></span>
-            <!-- ko if: rightPercentage() === null -->
-            <span title="Требуется проверка" class="tagged-label fa">&#xf123;</span>
+    <div class="layer">
+        <div class="details" data-bind="foreach: current.answers">
+            <div class="details-row special-item" data-bind="click: $root.actions.answer.show, css: {'current': $root.current.answer().id() === id()}">
+                <div class="details-column width-98p">
+                    <!-- ko if: rightPercentage() === null -->
+                    <span title="Требуется проверка" class="tagged fa">&#xf123;</span>
+                    <!-- /ko -->
+                    <span data-bind="text: $root.actions.answer.fit.question($data)"></span>
+                </div>
+            </div>
+            <!-- ko if: $root.current.answer().id() === id() -->
+            <div class="details-row special-item-details" data-bind="with: $root.current.answer">
+                <div class="details-column float-right">
+                    <div>
+                        <label class="title">Правильность ответа</label>
+                        <!-- ko if: rightPercentage() !== null && !$root.current.mark.isInput() -->
+                        <span class="radio-important" data-bind="text: rightPercentage, click: $root.actions.mark.edit"></span>
+                        <!-- /ko -->
+                        <!-- ko if: rightPercentage() === null && !$root.current.mark.isInput() -->
+                        <span class="radio-important" data-bind="text: $root.current.mark.value, click: $root.actions.mark.edit"></span>
+                        <!-- /ko -->
+                        <!-- ko if: $root.current.mark.isInput() -->
+                        <input class="text-center" type="text" data-bind="value: $root.current.mark.value">
+                        <span class="fa radio-important" data-bind="click: $root.actions.mark.approve">&#xf00c;</span>
+                        <span class="fa radio-important" data-bind="click: $root.actions.mark.cancel">&#xf00d;</span>
+                        <!-- /ko -->
+                    </div>
+                </div>
+                <div class="details-column width-80p">
+                    <div class="details-row">
+                        <div class="details-column">
+                            <span class="fa icon">&#xf128;</span>
+                            <span class="text" data-bind="text: question().text"></span>
+                        </div>
+                    </div>
+                    <div class="details-column">
+                        <span class="fa icon">&#xf24a;</span>
+                        <span class="text" class="text" data-bind="text: answer"></span>
+                    </div>
+                </div>
+            </div>
             <!-- /ko -->
         </div>
-        <!-- ko if: $root.current.answer().id() === id() -->
-        <div class="answer-details" data-bind="with: $root.current.answer">
-            <div class="question">
-                <span class="fa icon">&#xf128;</span>
-                <span class="text" data-bind="text: question().text"></span>
-            </div>
-            <div class="answer-text">
-                <span class="fa icon">&#xf24a;</span>
-                <span class="text" data-bind="text: answer"></span>
-            </div>
-            <div class="mark">
-                <label>Правильность ответа</label></br>
-                <!-- ko if: rightPercentage() !== null && !$root.current.mark.isInput() -->
-                <span data-bind="text: rightPercentage, click: $root.actions.mark.edit"></span>
-                <!-- /ko -->
-                <!-- ko if: rightPercentage() === null && !$root.current.mark.isInput() -->
-                <span data-bind="text: $root.current.mark.value, click: $root.actions.mark.edit"></span>
-                <!-- /ko -->
-                <!-- ko if: $root.current.mark.isInput() -->
-                <input type="text" data-bind="value: $root.current.mark.value">
-                <span class="fa" data-bind="click: $root.actions.mark.approve">&#xf00c;</span>
-                <span class="fa" data-bind="click: $root.actions.mark.cancel">&#xf00d;</span>
-                <!-- /ko -->
-            </div>
-        </div>
-        <!-- /ko -->
     </div>
     <!-- /ko -->
 </div>
-
+@include('admin.shared.error-modal')
 @endsection
-
-<div class="g-hidden">
-    <div class="box-modal" id="errors-modal">
-        <div>
-            <div>
-                <span class="fa">&#xf071;</span>
-                <h3>Произошла ошибка</h3>
-                <h4 data-bind="text: $root.errors.message"></h4>
-            </div>
-            <div class="button-holder">
-                <button data-bind="click: $root.errors.accept">OK</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 <div class="g-hidden">
     <div class="box-modal" id="attempts-modal">
