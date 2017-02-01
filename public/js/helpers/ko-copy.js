f5a5a51715159d41712772b1af6583584498f153
nyanjii <khiv.natalia@gmail.com>
2017-02-01 22:03:17 +0300

    Add: парсер даты на отображение

--- public/js/helpers/ko-copy.js
+++ public/js/helpers/ko-copy.js
@@ -4,3 +4,20 @@
 ko.observable.fn.copy = function(data){
     this(ko.mapping.fromJS(ko.mapping.toJS(data)));
 };
+
+ko.observable.fn.parseDate = function(){
+    var date = new Date(this());
+    var options = {
+        timezone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+    };
+    date = date.toLocaleString("ru", options);
+    date = date.replace(',', ' ');
+
+    return date;
+};
