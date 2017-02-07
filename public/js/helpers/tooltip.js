c78c47bf7294bea3fa8d5e1652edba172e8ba417
nyanjii <khiv.natalia@gmail.com>
2017-02-07 22:56:04 +0300

    Added: Общий формат валидационных тултипов (кривой коммит)

--- public/js/helpers/tooltip.js
+++ public/js/helpers/tooltip.js
@@ -1,3 +1,29 @@
-/**
- * Created by nyanjii on 07.02.17.
- */
+var validationTooltip = function(init){
+    var selector = init.selector;
+
+    this.initialize = function(){
+        if (this.isInitialized()) return;
+
+        $(selector).tooltipster({
+            theme: 'tooltipster-light',
+            trigger: 'custom'
+        });
+    };
+    this.isInitialized = function(){
+        return $(selector).hasClass('tooltipstered');
+    };
+    this.open = function(){
+        this.initialize();
+        this.text();
+        $(selector).tooltipster('open');
+    };
+    this.text = function(){
+        $(selector).tooltipster('content', init.text());
+    };
+    this.close = function(){
+        if (!this.isInitialized()) return;
+        $(selector).tooltipster('close');
+    };
+
+    this.initialize();
+};
